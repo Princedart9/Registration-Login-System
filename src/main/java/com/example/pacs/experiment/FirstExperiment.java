a74aefd78888316fb50a9a650665bb4e6d5fe7d7
Princedart9 <princedart9d@gmail.com>
2023-06-25 17:43:31 +0530

    fourth Commit of the day!!

--- src/main/java/com/example/pacs/experiment/FirstExperiment.java
+++ src/main/java/com/example/pacs/experiment/FirstExperiment.java
@@ -2,11 +2,25 @@ package com.example.pacs.experiment;
 
 public class FirstExperiment {
 
-	public FirstExperiment() {
+	String aString;
+	String bString;
+	public FirstExperiment(String a, String b) {
+		this.aString=a;
+		this.bString=b;
+	}
+
+	@Override
+	public String toString() {
+		return "I am " + aString + " " + bString +"!!";
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello...........");
+		
+		FirstExperiment obj1= new FirstExperiment("Prince", "Kumar");
+		
+		System.out.println(obj1);
+		
 	}
 
 }
